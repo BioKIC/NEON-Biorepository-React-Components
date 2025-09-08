b12223cf9ce48550d7cdbe96d759f1e865db5906
Chandra Earl <sunray1@bellsouth.net>
2025-09-08 12:14:12 -0400

    change checklist link

--- src/biorepo_components/BiorepoChecklistsContent.jsx
+++ src/biorepo_components/BiorepoChecklistsContent.jsx
@@ -17,7 +17,7 @@ function renderNode(nodes: any[], depth = 0) {
   return nodes.map((node, index) => {
     const isLastChild = index === nodes.length - 1;
     const collidLink = node.collid
-      ? `../checklists/checklist.php?clid=${node.collid}`
+      ? `../neon/checklists/checklist.php?clid=${node.collid}`
       : null;
 
     const nodeContent = collidLink ? (
